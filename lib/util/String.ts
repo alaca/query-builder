1418c4487d5ecf618f3fa6b8d682f785de0dd0e8
alaca <ante.laca@gmail.com>
2022-06-25 12:37:29 +0200

    feature: add escape string util

---
+++ lib/util/String.ts
@@ -0,0 +1,41 @@
+const CHARS_REGEXP = /[\0\b\t\n\r\x1a\"\'\\]/g;
+const CHARS_MAP = {
+  '\0': '\\0',
+  '\b': '\\b',
+  '\t': '\\t',
+  '\n': '\\n',
+  '\r': '\\r',
+  '\x1a': '\\Z',
+  '"': '\\"',
+  '\'': '\\\'',
+  '\\': '\\\\'
+};
+
+export function escapeString(val) {
+  let index: number = CHARS_REGEXP.lastIndex = 0;
+  let escaped: string = '';
+  let match: RegExpExecArray;
+
+  if (val === null || val === undefined) {
+    return 'NULL';
+  }
+
+  if (Number.isInteger(val)) {
+    return val;
+  }
+
+  while ((match = CHARS_REGEXP.exec(val))) {
+    escaped += val.slice(index, match.index) + CHARS_MAP[match[0]];
+    index = CHARS_REGEXP.lastIndex;
+  }
+
+  if (index === 0) {
+    return `'${val}'`;
+  }
+
+  if (index < val.length) {
+    return `'${escaped + val.slice(index)}'`;
+  }
+
+  return `'${escaped}'`;
+}
