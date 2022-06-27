c16cc3e9c1d982e8cb50a6bfb1936e9473488a72
alaca <ante.laca@gmail.com>
2022-06-27 17:05:53 +0200

    feature: JoinCondition class

---
+++ src/query/clauses/JoinCondition.ts
@@ -0,0 +1,23 @@
+import {ComparisonOperators, JoinOperators} from '../../types';
+
+export class JoinCondition {
+  logicalOperator: string;
+  column1: string;
+  column2: string | number;
+  comparisonOperator: ComparisonOperators
+  quote: boolean;
+
+  constructor(
+    logicalOperator: JoinOperators,
+    column1: string,
+    column2: string | number,
+    comparisonOperator: ComparisonOperators = '=',
+    quote: boolean = false
+  ) {
+    this.logicalOperator = logicalOperator;
+    this.column1 = column1.trim();
+    this.column2 = column2;
+    this.comparisonOperator = comparisonOperator;
+    this.quote = quote;
+  }
+}
