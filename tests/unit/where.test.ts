6fe597c73388653d2c4ab38d98f96338dde5e43c
alaca <ante.laca@gmail.com>
2022-06-28 11:47:35 +0200

    feature: where test

---
+++ tests/unit/where.test.ts
@@ -0,0 +1,10 @@
+import {QueryBuilder} from '../../src';
+
+test('select where', () => {
+  const sql = (new QueryBuilder())
+    .from('table')
+    .where('id', 10)
+    .getSQL();
+
+  expect(sql).toBe('SELECT * FROM table WHERE id = 10');
+});
