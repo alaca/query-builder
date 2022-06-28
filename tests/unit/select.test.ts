557f7e8f65aa2780a0d2299b161ceb85258fd55a
alaca <ante.laca@gmail.com>
2022-06-28 11:47:08 +0200

    feature: select test

---
+++ tests/unit/select.test.ts
@@ -0,0 +1,47 @@
+import {QueryBuilder} from '../../src';
+
+test('select specific columns from table', () => {
+  const sql = (new QueryBuilder())
+    .select('id', 'name', 'age')
+    .from('table')
+    .getSQL();
+
+  expect(sql).toBe('SELECT id, name, age FROM table');
+});
+
+test('select all columns from table', () => {
+  const sql = (new QueryBuilder())
+    .from('table')
+    .getSQL();
+
+  expect(sql).toBe('SELECT * FROM table');
+});
+
+test('select raw statement', () => {
+  const sql = (new QueryBuilder())
+    .selectRaw('SELECT column_one, column_two')
+    .from('table')
+    .getSQL();
+
+  expect(sql).toBe('SELECT column_one, column_two FROM table');
+});
+
+
+test('select column alias', () => {
+  const sql = (new QueryBuilder())
+    .select({id: 'product_id'})
+    .from('table')
+    .getSQL();
+
+  expect(sql).toBe('SELECT id AS product_id FROM table');
+});
+
+test('select distinct', () => {
+  const sql = (new QueryBuilder())
+    .distinct()
+    .select('id')
+    .from('table')
+    .getSQL();
+
+  expect(sql).toBe('SELECT DISTINCT id FROM table');
+});
